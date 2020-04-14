2fdada73c2f32d7ac7dce7254e294f56b7b58413
Michal Tichý <email@tichymichal.net>
2020-04-14 22:22:52 +0200

    reworked default page

--- TestSamples/ViewModels/DefaultViewModel.cs
+++ TestSamples/ViewModels/DefaultViewModel.cs
@@ -8,18 +8,20 @@ namespace TestSamples.ViewModels
     public class DefaultViewModel : DotvvmViewModelBase
     {
         public string Title { get; set; }
-        public List<RouteData> Routes { get; set; }
+        public List<RouteData> NormalRoutes { get; set; }
+        public List<RouteData> AmpRoutes { get; set; }
 
         public override Task Init()
         {
-            Routes = Context.Configuration.RouteTable
+            var routes = Context.Configuration.RouteTable
                 .Select(r => new RouteData()
                 {
                     RouteName = r.RouteName,
                     Url = Context.TranslateVirtualPath(r.BuildUrl(r.DefaultValues))
                 })
                 .ToList();
-
+            NormalRoutes = routes.Where(t => !t.RouteName.EndsWith("-amp")).ToList();
+            AmpRoutes = routes.Where(t => t.RouteName.EndsWith("-amp")).ToList();
             return base.Init();
         }
 
