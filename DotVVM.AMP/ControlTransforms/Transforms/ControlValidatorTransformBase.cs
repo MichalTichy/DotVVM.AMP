f5e79607c3421fe7a4d8097204e342ea6c6c7d78
Michal Tichý <email@tichymichal.net>
2020-04-20 21:47:19 +0200

    implemented IsPropertySupported method on ControlValidatorTransformBase

--- DotVVM.AMP/ControlTransforms/Transforms/ControlValidatorTransformBase.cs
+++ DotVVM.AMP/ControlTransforms/Transforms/ControlValidatorTransformBase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using DotVVM.AMP.Config;
+using DotVVM.AMP.Enums;
+using DotVVM.AMP.Validator;
 using DotVVM.Framework.Controls;
 using DotVVM.Framework.Hosting;
 using Microsoft.Extensions.Logging;
@@ -19,8 +21,22 @@ namespace DotVVM.AMP.ControlTransforms.Transforms
 
         protected void IsPropertySupported(TControl control, Func<TControl, bool> IsSupported, Action<TControl> setToSupportedState, string propertyName)
         {
+            if (IsSupported(control))
+                return;
+            
             var message = $"Property {propertyName} is not supported";
 
+            switch (AmpConfiguration.UnsupportedControlPropertiesHandlingMode)
+            {
+                case ErrorHandlingMode.Throw:
+                    throw new AmpException(message);
+                case ErrorHandlingMode.LogAndIgnore:
+                    Logger?.LogError(message);
+                    setToSupportedState(control);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
 
         }
 
